8ca8337cff6c3b2bb7290035171d0655af3a6824
Vitaliy Nastosiak <31761508+Arius1044@users.noreply.github.com>
2017-10-26 21:42:44 +0300

    v0.101

--- Airport.cpp
+++ Airport.cpp
@@ -2,11 +2,11 @@
 #include "Airport.h";
 using namespace std;
 
-Airport::Airport() : name("incognito"), country("incognito"), city("incognito"), contact_number("incognito")
+Airport::Airport() : name("incognito"), country("incognito"), city("incognito"), contact_number(0)
 {
 }
 
-Airport::Airport(string _name_, string _country_, string _city_, string  _contact_number_) : name(_name_), country(_country_), city(_city_),contact_number(_contact_number_)
+Airport::Airport(string _name_, string _country_, string _city_, long long  _contact_number_) : name(_name_), country(_country_), city(_city_),contact_number(_contact_number_)
 {
 }
 
@@ -25,9 +25,9 @@ void Airport::set_Airport_city(string _city_)
 	city.assign(_city_);
 }
 
-void Airport::set_Airport_contact_number(string _contact_number_)
+void Airport::set_Airport_contact_number(long long _contact_number_)
 {
-	contact_number.assign(_contact_number_);
+	contact_number = _contact_number_;
 }
 
 string Airport::get_Airport_name()const
@@ -45,7 +45,7 @@ string Airport::get_Airport_city()const
 	return city;
 }
 
-string Airport::get_Airport_contact_number()const
+long long Airport::get_Airport_contact_number()const
 {
 	return contact_number;
 }
