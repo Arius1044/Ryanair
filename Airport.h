15be10b3f8b67ef65c90374da3a4cd5bc4d5adbf
Vitaliy Nastosiak <31761508+Arius1044@users.noreply.github.com>
2017-10-26 21:40:45 +0300

    v0.1

--- Airport.h
+++ Airport.h
@@ -8,7 +8,7 @@ private:
 	string name;
 	string country;
 	string city;
-	string contact_number;
+	long long contact_number;
 public:
 	Airport();
 	Airport(string _name_, string _country_, string _city_, string _contact_number_);
@@ -16,12 +16,12 @@ public:
 	void set_Airport_name(string _name_);
 	void set_Airport_country(string _country_);
 	void set_Airport_city(string _city_);
-	void set_Airport_contact_number(string _contact_number_);
+	void set_Airport_contact_number(long long _contact_number_);
 
 	string get_Airport_name()const;
 	string get_Airport_country()const;
 	string get_Airport_city()const;
-	string get_Airport_contact_number()const;
+	long long get_Airport_contact_number()const;
 
 	void print_info_airport()const;
 	~Airport();
